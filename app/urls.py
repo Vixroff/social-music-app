4186cf5fa666e9b7c6d4fff45eed297263750ad3
Maksim Vikhrov <maxvihr@yandex.ru>
2023-02-28 14:46:32 +0300

    Refactored: urls look much better

--- app/urls.py
+++ app/urls.py
@@ -6,14 +6,20 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.MyLoginView.as_view(), name='login'),
-    path('', include('django.contrib.auth.urls')),
-    path('add_track', views.add_track, name='add_track'),
-    path('create-playlist/', views.CreatePlaylistView.as_view(), name='create_playlist'), 
-    path('playlist/<int:pk>', views.playlist, name='playlist'),
-    path('profile/', views.profile, name='profile'),
-    path('register/', views.RegistrationView.as_view(), name='registration'),
-    path('search', views.SearchView.as_view(), name='search'),
-    path('top-artists', views.TopArtistsView.as_view(), name='top_artists'),
-    path('top-tracks', views.TopTracksView.as_view(), name='top_tracks'),    
+    path('accounts/', include([
+        path('login/', views.MyLoginView.as_view(), name='login'),
+        path('profile/', views.profile, name='profile'),
+        path('register/', views.RegistrationView.as_view(), name='registration'), 
+        path('', include('django.contrib.auth.urls')),
+    ])),
+    path('content/', include([
+        path('create-playlist/', views.CreatePlaylistView.as_view(), name='create_playlist'), 
+        path('playlist/<int:pk>/', views.playlist, name='playlist'),
+    ])),
+    path('search/', include([
+        path('search', views.SearchView.as_view(), name='search'),
+        path('top-artists', views.TopArtistsView.as_view(), name='top_artists'),
+        path('top-tracks', views.TopTracksView.as_view(), name='top_tracks'), 
+    ])),
+    path('add_track', views.add_track, name='add_track'),        
 ]
